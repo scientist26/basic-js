97064d3b30ddd7a16ece057530ba6f366a786a7c
Andrei Bezmen <andryshabez26@gmail.com>
2020-03-03 23:38:56 +0300

    feat: add solution to count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,12 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+
+  let ears = "^^";
+  let firstLevelArr = [].concat(...matrix);
+  let count = 0;
+  for (let cat of firstLevelArr) {
+    if (cat === ears) {
+      count++
+    }
+  }
+  return count
 };
