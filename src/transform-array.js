41c8f21dca194954dfc6294a563e359b40970f81
Andrei Bezmen <andryshabez26@gmail.com>
2020-03-08 19:44:41 +0300

    feat: add solution to transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,32 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  // console.log(JSON.stringify(arr))
+  if (arr.length === 0) {
+    return arr
+  }
+
+  if (!Array.isArray(arr) || typeof(arr) !== 'object') {
+    throw new Error()
+  }
+
+  let transformArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+        i += 1;
+    } else if (arr[i] === '--discard-prev') {
+        if (transformArr.length > 0) {
+          transformArr.pop();
+        }
+    } else if (arr[i] === '--double-next') {
+      if (i < arr.length - 1) {
+        transformArr.push(arr[i + 1]);
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (i > 0) {
+        transformArr.push(arr[i - 1]);
+      }
+    } else {
+      transformArr.push(arr[i])
+  }
+  }
+  return transformArr;
 };
